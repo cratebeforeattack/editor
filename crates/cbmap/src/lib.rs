45b2356d521fe35adc9238fd81c3a5393d79811c
koalefant <koalefant@fastmail.com>
2021-10-10 21:18:57 +0200

    cbmap: use Option for optional parameters instead of a default

--- crates/cbmap/src/lib.rs
+++ crates/cbmap/src/lib.rs
@@ -1,62 +1,50 @@
-mod material;
 mod markup;
+mod material;
 
-pub use material::*;
 pub use markup::*;
-use serde_derive::{Deserialize, Serialize};
-
+pub use material::*;
+use serde_derive::Deserialize;
 
 #[derive(Deserialize, Clone)]
 pub struct MapLook {
-    #[serde(default = "MapLook::default_background_color")]
-    pub background_color: [u8; 3],
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub background_color: Option<[u8; 3]>,
 
-    #[serde(default = "MapLook::default_water_opacity")]
-    pub water_opacity: u8,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub water_opacity: Option<u8>,
 
-    #[serde(default = "MapLook::default_water_color_outline")]
-    pub water_color_outline: [u8; 3],
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub water_color_outline: Option<[u8; 3]>,
 
-    #[serde(default = "MapLook::default_water_color_shallow")]
-    pub water_color_shallow: [u8; 3],
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub water_color_shallow: Option<[u8; 3]>,
 
-    #[serde(default = "MapLook::default_water_color_deep")]
-    pub water_color_deep: [u8; 3],
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub water_color_deep: Option<[u8; 3]>,
 
     /// Areas where pawn spawn is prohibited
     #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub no_player_spawn: Vec<[i32; 4]>,
 
     #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub markup: Option<MapMarkup>,
-}
-
-impl MapLook {
-    fn default_background_color() -> [u8; 3] {
-        [0, 0, 0]
-    }
-    fn default_water_opacity() -> u8 {
-        127
-    }
-    fn default_water_color_shallow() -> [u8; 3] {
-        [0, 0, 64]
-    }
-    fn default_water_color_deep() -> [u8; 3] {
-        [0, 0, 64]
-    }
-    fn default_water_color_outline() -> [u8; 3] {
-        [76, 76, 255]
-    }
 }
 
 impl Default for MapLook {
     fn default() -> MapLook {
         MapLook {
-            background_color: Self::default_background_color(),
-            water_opacity: Self::default_water_opacity(),
-            water_color_shallow: Self::default_water_color_shallow(),
-            water_color_deep: Self::default_water_color_deep(),
-            water_color_outline: Self::default_water_color_outline(),
+            background_color: None,
+            water_opacity: None,
+            water_color_shallow: None,
+            water_color_deep: None,
+            water_color_outline: None,
             no_player_spawn: Vec::new(),
             markup: None,
         }
