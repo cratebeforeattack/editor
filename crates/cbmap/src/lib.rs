39c9ddfc124655c4ee1adf8f234f1b8b327b7733
koalefant <koalefant@fastmail.com>
2021-10-10 20:54:53 +0200

    cbmap: added markup types

--- crates/cbmap/src/lib.rs
+++ crates/cbmap/src/lib.rs
@@ -1,2 +1,64 @@
 mod material;
+mod markup;
+
 pub use material::*;
+pub use markup::*;
+use serde_derive::{Deserialize, Serialize};
+
+
+#[derive(Deserialize, Clone)]
+pub struct MapLook {
+    #[serde(default = "MapLook::default_background_color")]
+    pub background_color: [u8; 3],
+
+    #[serde(default = "MapLook::default_water_opacity")]
+    pub water_opacity: u8,
+
+    #[serde(default = "MapLook::default_water_color_outline")]
+    pub water_color_outline: [u8; 3],
+
+    #[serde(default = "MapLook::default_water_color_shallow")]
+    pub water_color_shallow: [u8; 3],
+
+    #[serde(default = "MapLook::default_water_color_deep")]
+    pub water_color_deep: [u8; 3],
+
+    /// Areas where pawn spawn is prohibited
+    #[serde(default)]
+    pub no_player_spawn: Vec<[i32; 4]>,
+
+    #[serde(default)]
+    pub markup: Option<MapMarkup>,
+}
+
+impl MapLook {
+    fn default_background_color() -> [u8; 3] {
+        [0, 0, 0]
+    }
+    fn default_water_opacity() -> u8 {
+        127
+    }
+    fn default_water_color_shallow() -> [u8; 3] {
+        [0, 0, 64]
+    }
+    fn default_water_color_deep() -> [u8; 3] {
+        [0, 0, 64]
+    }
+    fn default_water_color_outline() -> [u8; 3] {
+        [76, 76, 255]
+    }
+}
+
+impl Default for MapLook {
+    fn default() -> MapLook {
+        MapLook {
+            background_color: Self::default_background_color(),
+            water_opacity: Self::default_water_opacity(),
+            water_color_shallow: Self::default_water_color_shallow(),
+            water_color_deep: Self::default_water_color_deep(),
+            water_color_outline: Self::default_water_color_outline(),
+            no_player_spawn: Vec::new(),
+            markup: None,
+        }
+    }
+}
---
+++ crates/cbmap/src/markup.rs
@@ -0,0 +1,49 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Copy, Debug, Clone, Hash, Serialize, Deserialize, PartialEq)]
+pub enum MarkupPointKind {
+    Start,
+}
+
+#[derive(Copy, Debug, Clone, Hash, Serialize, Deserialize, PartialEq)]
+pub enum MarkupRectKind {
+    RaceFinish,
+}
+
+#[derive(Copy, Debug, Clone, Hash, Serialize, Deserialize, PartialEq)]
+pub struct MarkupPoint {
+    pub kind: MarkupPointKind,
+    pub pos: [i32; 2],
+}
+
+#[derive(Copy, Debug, Clone, Hash, Serialize, Deserialize, PartialEq)]
+pub struct MarkupRect {
+    pub kind: MarkupRectKind,
+    pub start: [i32; 2],
+    pub end: [i32; 2],
+}
+
+
+#[derive(Debug, Clone, Hash, Serialize, Deserialize, PartialEq)]
+pub struct MapMarkup {
+    pub points: Vec<MarkupPoint>,
+    pub rects: Vec<MarkupRect>,
+}
+
+impl Default for MapMarkup {
+    fn default() -> Self {
+        Self {
+            points: Vec::new(),
+            rects: Vec::new(),
+        }
+    }
+}
+
+impl MapMarkup {
+    pub fn new() -> Self {
+        Self {
+            points: Vec::new(),
+            rects: Vec::new(),
+        }
+    }
+}
